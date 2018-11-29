50c4655067f8fe3982ee5517592009b2377874b5
J. Famous <joshua.famous@gmail.com>
2018-11-29 01:38:41 -0500

    Working Project display with selectors

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 function loadCourses(element){
-
 	var courseType = element.getAttribute('data-courseType');
 	var courses = document.getElementsByClassName("courseSection");
 	for(var i = 0; i < courses.length; i++)
@@ -12,4 +11,20 @@ function loadCourses(element){
 	   		thisCourse.style.display = "none";
 	   	}
 	}
+}
+
+function loadProject(element){
+	var projectTitle = element.getAttribute('data-projectTitle');
+	var projects = document.getElementsByClassName("projectSection");
+	for(var i = 0; i < projects.length; i++)
+	{
+		var thisProject = projects[i];
+	  	if(thisProject.getAttribute('data-projectTitle') == projectTitle){
+	  		thisProject.style.display = "block";
+	   	}
+	   	else{
+	   		thisProject.style.display = "none";
+	   	}
+	}
+	document.getElementById("projectImage").src = element.getAttribute('data-imageURL');
 }
